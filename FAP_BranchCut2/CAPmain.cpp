4fd6b14d7a9cf1f25c5b06d7cd80048e39936a28
kasiatikhoncheva@gmail.com <kasiatikhoncheva@gmail.com>
2014-09-15 15:59:44 +0200

    same, as FAP_BranchCut, but with classical formultation of the problem

---
+++ FAP_BranchCut2/CAPmain.cpp
@@ -0,0 +1,94 @@
+//============================================================================
+// Name        : CAPmain.cpp
+// Author      : 
+// Version     :
+// Comments    : main fuction
+//============================================================================
+
+#include <iostream>
+#include <iomanip>
+
+#include "CAP.cpp"
+#include "var.h"
+
+static void usage(const char *progname)
+{
+	std::cerr << "Usage: " << progname << " <d max> <lattice> <filename>"
+			<< std::endl;
+	std::cerr << "   where  <d max> is d_1 for L(d_1,d_1-1,..,1)-Labeling"
+			<< std::endl;
+	std::cerr << "   <lattice>= 1 for hexagonal lattice;"
+			<< " 2 for triangular lattice; 3 for square lattice and "
+			<< " 0 for arbitrary graph defined in inputfile" << std::endl;
+
+	std::cerr
+			<< "   <filename> is a file with distance matrix and number of vertices in graph "
+			<< std::endl;
+	std::cerr << " Exiting..." << std::endl;
+} // END usage
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		if (argc != 4 && atoi(argv[2]) == 0)
+		{
+			usage(argv[0]);
+			throw(-1);
+		}
+
+		max_dist = atoi(argv[1]); // d_1
+		CProblemData* inputData;
+
+		if (atoi(argv[2]) == 0) // if we want read from file
+		{
+			std::string fileName = argv[3];
+			std::cout << "\n\nStart with the file " << fileName << "\n";
+			inputData = new CProblemData(fileName);
+		}
+		else // we work with a predefined lattice graph
+		{
+			inputData = new CProblemData(atoi(argv[2]) /*lattice typ*/);
+		}
+
+		//inputData->print();
+
+		/*
+		 std::vector<std::set<int> > cliqueList = findMaxClique(d);
+
+		 std::cout << "Maximal clique in the given Graph " << std::endl;
+
+		 for (unsigned int i = 0; i < cliqueList.size(); i++)
+		 {
+		 std::set<int>::iterator itC;
+		 std::set<int> C = cliqueList[i];
+
+		 std::cout << "Clique " << i + 1 << ": ";
+
+		 for (itC = C.begin(); itC != C.end(); itC++)
+		 {
+		 std::cout << *itC << "<->";
+		 }
+		 std::cout << "|" << std::endl;
+		 }
+		 */
+
+		MIP_Problem CAP(max_dist, inputData); // define new CAP Problem
+		CAP.print(); // print problem
+
+		CAP.solve(); // solve problem
+		CAP.printSolution(); // print solution on the screen
+		CAP.saveSolutionToFile(inputData->getFileName()); // save solution in file
+
+	} // END try
+	catch (int& e)
+	{
+		std::cerr << "Error in input parameters " << e << std::endl;
+	} catch (...)
+	{
+		std::cerr << "Unknown exception caught" << std::endl;
+	}
+
+	return 0;
+}
+
---
+++ FAP_BranchCut2/ProblemData.h
@@ -0,0 +1,254 @@
+#ifndef PROBLEMDATA_H_
+#define PROBLEMDATA_H_
+
+#include<string>
+#include<vector>
+#include"var.h"
+
+/*
+ typedef std::vector<double> VectorDouble;
+ typedef std::vector<VectorDouble> MatrixDouble;
+ */
+
+MatrixDouble hexagonalLattice(int n, int n1)
+{
+	MatrixDouble coord;
+	return coord;
+}
+
+MatrixDouble triangularLattice(int n, int n1)
+{
+	MatrixDouble coord;
+	return coord;
+}
+
+MatrixDouble squareLattice(int n, int n1)
+{
+	MatrixDouble coord;
+	return coord;
+}
+
+MatrixDouble squareCoordToDist(MatrixDouble coord)
+{
+	// simply city block distance
+	MatrixDouble distance;
+	distance.resize(coord.size());
+
+	for (unsigned int u; u < coord.size(); u++)
+	{
+		distance[u].resize(coord.size());
+		for (unsigned int v; v < u; v++)
+		{
+			distance[u][v] = std::abs(coord[u][0] - coord[v][0])
+					+ std::abs(coord[u][1] - coord[v][1]);
+			//distance[v][u] = distance[u][v];
+		}
+
+		distance[u][u] = 0;
+	}
+
+	std::cout << "Distances: " << "\n";
+	for (unsigned int u = 0; u < distance.size(); u++)
+	{
+		for (unsigned int v = 0; v < distance[u].size(); v++)
+		{
+			std::cout << std::setw(3) << distance[u][v] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	return distance;
+}
+
+class CProblemData
+{
+
+private:
+	int lattice; // lattice type, if we consider a lattice graph
+	std::string fileName; // name of the file with distance matrix
+	int readData();
+	//void coordTOdist();
+	int dist_linear(int x);
+	void set_d_function();
+
+public:
+	unsigned int n; // number of vertices
+	//unsigned int diameter; // diameter of the graph
+	unsigned int maxGrad; // maximal grade of a graph
+	MatrixDouble dist; // distance matrix
+
+	void print();
+	const std::string getFileName()
+	{
+		unsigned found = fileName.find_last_of("/");
+
+		if (found != 0)
+			fileName = fileName.substr(found + 1,
+					fileName.length() - 1 - found);
+
+		return fileName;
+	}
+
+	CProblemData()
+	{
+		lattice = 0;
+		maxGrad = 0;
+		fileName = "dumm.txt";
+	}
+
+	CProblemData(std::string fileName_) :
+			fileName(fileName_)
+	{
+		lattice = 0;
+		readData();
+	}
+
+	CProblemData(int lattice_) :
+			lattice(lattice_)
+	{
+		fileName = "WeWorkWithLatticeGraph.txt";
+		readData();
+	}
+
+	~CProblemData()
+	{
+	}
+	;
+
+};
+
+int CProblemData::readData()
+{
+	std::string line;
+	std::fstream oFile;
+	std::stringstream buf;
+
+	switch (lattice)
+	{
+	case 0:
+		oFile.open(fileName.c_str());
+		if (!oFile.is_open())
+		{
+			std::cerr << "Cannot open file!\n";
+			exit(1);
+		}
+
+		std::getline(oFile, line); //number of vertices and diameter
+		buf << line;
+		buf >> n;
+		//buf >> diameter;
+		buf >> maxGrad;
+		buf.clear();
+
+		std::getline(oFile, line);
+
+		dist.resize(n);
+		for (unsigned int i = 0; i < n; i++)
+		{
+			dist[i].resize(n);
+			dist[i][i] = 0;
+
+			std::getline(oFile, line);
+
+			std::stringstream buf(line);
+			int int_buf;
+
+			for (unsigned int j = 0; j < i; j++)
+			{
+				buf >> int_buf;
+				//distanceVektor.push_back(int_buf);
+				dist[i][j] = int_buf;
+				dist[j][i] = dist[i][j];
+			}
+			buf.clear(); //clear buf
+
+		}
+
+		oFile.close();
+		break;
+
+	case 1:
+		n = 24;
+		dist = hexagonalLattice(n, 2); // number of vertices, number of vertices in first line
+		//diameter = 7;
+		maxGrad = 3;
+		fileName = "hexagonal_lattice.dat";
+		break;
+
+	case 2:
+		n = 23;
+		dist = triangularLattice(n, 5); // number of vertices, number of vertices in first line
+		// diameter = 6;
+		maxGrad = 6;
+		fileName = "triangular_lattice.dat";
+		break;
+
+	case 3:
+		n = 25;
+		//diameter = 8;
+		maxGrad = 4;
+		dist = squareCoordToDist(squareLattice(n, sqrt(n)));
+
+		fileName = "square_lattice.dat";
+		break;
+
+	default:
+		std::cerr << "Wrong lattice type\n";
+		exit(-1);
+	} //switch
+
+	set_d_function();
+
+	return 0;
+}
+
+void CProblemData::print()
+{
+	std::cout << "\n";
+	std::cout << "FileName: " << fileName << std::endl;
+	std::cout << "Graph (V,E) with |V|= " << n << " and maxGrade = " << maxGrad
+			<< "\n";
+	std::cout << "\n";
+
+	std::cout << "Distances: " << "\n";
+	for (unsigned int u = 0; u < n; u++)
+	{
+		for (unsigned int v = 0; v < n; v++)
+		{
+			std::cout << std::setw(3) << dist[u][v] << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
+int CProblemData::dist_linear(int x)
+{
+	int value = 1 + max_dist - x;
+	return (value < 0) ? 0 : value; /* linear function of dist*/
+	//return value;
+}
+
+void CProblemData::set_d_function()
+{
+	d.resize(n);
+	for (unsigned int u = 0; u < n; u++)
+	{
+		d[u].resize(n);
+		for (unsigned int v = 0; v < u; v++)
+		{
+			d[u][v] = dist_linear(dist[u][v]);
+			d[v][u] = d[u][v];
+
+			if (dist[u][v] <= max_dist && u != v)
+			{ // save vertices for which interferent with each other
+				info.InterferenceVertices.push_back(u);
+				info.InterferenceVertices.push_back(v);
+			}
+
+		}
+		//d[u][u] = max_dist + 1;
+	}
+
+}
+
+#endif /* PROBLEMDATA_H_ */
