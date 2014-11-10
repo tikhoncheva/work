6cb3b5657f40f961178625c4dc239d4e83eaf5df
kasiatikhoncheva@gmail.com <kasiatikhoncheva@gmail.com>
2014-11-10 17:01:56 +0100

    start of the work on the Nouna Project

---
+++ Nouna_PCVRP/ver00/Nouna_PCVRP.cpp
@@ -0,0 +1,37 @@
+//============================================================================
+// Name        : Nouna_PCVRP.cpp
+// Author      : Ekaterina Tikhoncheva
+// Version     : 1.0
+//============================================================================
+
+#include <iostream>
+#include <array>
+#include "datadef.h"
+#include "readdata.h"
+
+/*
+ * correct call format
+ */
+void usage()
+{
+
+}
+
+int main()
+{
+	int error;
+
+	std::vector<std::string> fileName(3);
+	fileName[0] = "../data/villages.csv";
+	fileName[1] = "../data/routes.csv";
+	fileName[2] = "../data/households.csv";
+
+	// read data
+	error = readdata_csv(fileName);
+
+	Houshold[0].print();
+	Village[0].print();
+	Route[0].print();
+	std::cout << "\nFinished main" << std::endl;
+	return 0;
+}
---
+++ Nouna_PCVRP/ver00/datadef.h
@@ -0,0 +1,80 @@
+#ifndef DATADEF_H_
+#define DATADEF_H_
+
+#include <vector>
+#include <utility>
+
+/*
+ * A household
+ */
+struct stHousehold
+{
+	unsigned int ID;
+	std::string name;
+	unsigned int type; // 1 or 0
+	unsigned int nPersons;
+	unsigned int villageID;
+
+	void print()
+	{
+		std::cout << "ID=" << ID << " name=" << name << "  type=" << type
+				<< "  nPersons=" << nPersons << "  villageID=" << villageID
+				<< std::endl;
+	}
+};
+
+/*
+ * A Village
+ */
+struct stVillage
+{
+	unsigned int ID;
+	std::string name;
+	std::pair<double, double> coord; // longitude and latitude
+
+	void print()
+	{
+		std::cout << "ID=" << ID << " name=" << name << "  coord=("
+				<< coord.first << "," << coord.second << ")" << std::endl;
+	}
+};
+
+/*
+ * Route
+ */
+struct stRoute
+{
+	unsigned int ID;
+	std::string name;
+	unsigned int start;		// start village
+	unsigned int end;		// end village
+	double dist;
+	unsigned int category;	// 1, 2, 3, 4
+	unsigned int rain;
+
+	void print()
+	{
+		std::cout << "ID=" << ID << " name=" << name << "  from " << start
+				<< "  to " << end << "  dist=" << dist
+				<< "  category=" << category << "  rain=" << rain
+				<< std::endl;
+	}
+
+};
+
+/*
+ * An interviewer
+ */
+struct Interviewer
+{
+	unsigned int ID;
+};
+
+int N;		// number of households
+int K;		// number of interviewer
+
+std::vector<stVillage> Village;
+std::vector<stRoute> Route;
+std::vector<stHousehold> Houshold;
+
+#endif /* DATADEF_H_ */
