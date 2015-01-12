1ba5b525d466d3838152866860c7c2a1a2abc90d
kasiatikhoncheva@gmail.com <kasiatikhoncheva@gmail.com>
2015-01-12 13:13:05 +0100

    start new version of the project

---
+++ Nouna_PCVRP/qt-project/ver01/nouna_project/hrd/household.h
@@ -0,0 +1,24 @@
+#ifndef HOUSEHOLD_H
+#define HOUSEHOLD_H
+
+/*
+ * A household
+ */
+
+struct stHousehold
+{
+    unsigned int ID;
+    std::string name;
+    unsigned int type; // 1 or 0
+    unsigned int nPersons;
+    unsigned int villageID;
+
+    void print()
+    {
+        std::cout << "ID=" << ID << " name=" << name << "  type=" << type
+                << "  nPersons=" << nPersons << "  villageID=" << villageID
+                << std::endl;
+    }
+};
+
+#endif // HOUSEHOLD_H
